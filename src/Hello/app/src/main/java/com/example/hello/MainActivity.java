05dae94923e337d38691d484c221f8d684addfbc
luochenhen <41855935+luochenhen@users.noreply.github.com>
2019-09-19 08:37:41 +0800

    add login.xml and sign.xml. Ref #5

--- src/Hello/app/src/main/java/com/example/hello/MainActivity.java
+++ src/Hello/app/src/main/java/com/example/hello/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.hello;
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
@@ -7,14 +8,21 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONObject;
+
+import java.util.List;
 
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -24,67 +32,51 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView;
+    private Spinner spinner;
+    private List<String> list;
+    private ArrayAdapter<String> adapter;
     EditText e1, e2;
     ImageView m1, m2;
     Button bt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("MainActivity", "*&*&*&*&*&*&*&*&*&*");
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_user_login);
+        setContentView(R.layout.user_login);
+
+        Resources res =getResources();
+        final String[] ways=res.getStringArray(R.array.signway);//将province中内容添加到数组city中
+        spinner = (Spinner) findViewById(R.id.spinner_login);//获取到spacer1
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,ways);//创建Arrayadapter适配器
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {//通过此方法为下拉列表设置点击事件
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i,long l) {
+                String text= spinner.getItemAtPosition(i).toString();
+                for(int j=0;j<ways.length;j++){
+                    if(text==ways[j]){
+                        System.out.println(j);
+                        break;
+                    }
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+
+//        String[] ctype = new String[]{"手机号", "邮箱", "用户名"};
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, ctype);  //创建一个数组适配器
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);     //设置下拉列表框的下拉选项样式
+//        Spinner spinner = super.findViewById(R.id.spinner_login);
+//        spinner.setSelection(1);
+//
+//        spinner.setAdapter(adapter);
+        //System.out.println(spinner.getSelectedItem().toString());
         if (android.os.Build.VERSION.SDK_INT > 9) {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
         }
-
-        init();
-        bt.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                JSONObject jsonObject = new JSONObject();
-                String name = e1.getText().toString();
-                String pwd = e2.getText().toString();
-                try {
-                    jsonObject.put("name", name);
-                    jsonObject.put("pw", pwd);
-                } catch (Exception e) {
-                    System.out.println("error");
-                    System.out.println(e);
-                }
-                OkHttpClient client = new OkHttpClient();//创建OkHttpClient对象。
-                MediaType JSON = MediaType.parse("application/json; charset=utf-8");//数据类型为json格式，
-                String jsonStr = "{\"name\":\"xiaoming\",\"pw\":\"123\"}";//json数据.
-                RequestBody body = RequestBody.create(JSON, jsonObject.toString());
-                System.out.println(jsonObject.toString());
-                Request request = new Request.Builder()
-                        .url("http://10.0.2.2:8000/user/login/")
-                        .post(body)
-                        .build();
-                try {
-                    Response response = client.newCall(request).execute();
-                    String sss = response.body().string();
-                    System.out.println("Sucess");
-                    System.out.println(sss);
-                }
-                catch (Exception e) {
-                    System.out.println(e);
-                }
-            }
-        });
-    }
-
-    private void init() {
-        // TODO Auto-generated method stub
-        e1 = (EditText) findViewById(R.id.phonenumber);
-        e2 = (EditText) findViewById(R.id.password);
-        m1 = (ImageView) findViewById(R.id.del_phonenumber);
-        m2 = (ImageView) findViewById(R.id.del_password);
-        bt = (Button)findViewById(R.id.loginButton);
-
-//�������������
-        EditTextClearTools.addclerListener(e1, m1);
-        EditTextClearTools.addclerListener(e2, m2);
-        System.out.println(e1.getText().toString());
     }
 }
